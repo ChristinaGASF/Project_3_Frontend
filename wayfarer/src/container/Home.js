bf3c93ed318bae34c422437563a0a88f7c702a54
ChristinaGASF <44411722+ChristinaGASF@users.noreply.github.com>
2019-01-11 07:22:11 -0800

    content added

--- wayfarer/src/container/Home.js
+++ wayfarer/src/container/Home.js
@@ -7,16 +7,22 @@ class Home extends Component{
         return(
             <React.Fragment>
             <Hero />
+            <div>
+                <h4>Wayfarer is a travel site for users to share tips about their favorite museums, historical sites and  landmark iconic city spots. </h4>
+            </div>
             <Grid>
                 <Row className="show-grid">
                 <Col xs={6} md={4}>
-                <p>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil ad consectetur necessitatibus quisquam provident qui beatae quibusdam natus illo, officiis fuga deleniti, aperiam dolorum deserunt est, aspernatur ea ipsum modi?'</p>
+                <h5>Museums</h5>
+                <p>There are many types of museums, including art museums, natural history museums, science museums, war museums, and children's museums. </p>
                 </Col>
                 <Col xs={6} md={4}>
-                <p>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil ad consectetur necessitatibus quisquam provident qui beatae quibusdam natus illo, officiis fuga deleniti, aperiam dolorum deserunt est, aspernatur ea ipsum modi?'</p>
+                <h5>Historical Sites</h5>
+                <p>Historic sites are official locations where pieces of political, military, cultural, or social history have been preserved due to their cultural heritage value.</p>
                 </Col>
                 <Col xs={6} md={4}>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil ad consectetur necessitatibus quisquam provident qui beatae quibusdam natus illo, officiis fuga deleniti, aperiam dolorum deserunt est, aspernatur ea ipsum modi?</p>
+                <h5>Landmarks</h5>
+                <p>A landmark is a recognizable natural or artificial feature used for navigation, a feature that stands out from its near environment and is often visible from long distances.</p>
                 </Col>
                 </Row></Grid>
             </React.Fragment>
