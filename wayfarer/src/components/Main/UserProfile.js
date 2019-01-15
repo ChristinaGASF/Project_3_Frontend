a14002e9c7b97f7d10a6edc1c84639358918cf60
ChristinaGASF <44411722+ChristinaGASF@users.noreply.github.com>
2019-01-15 13:20:56 -0800

    update user functionality

--- wayfarer/src/components/Main/UserProfile.js
+++ wayfarer/src/components/Main/UserProfile.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { Form , FormGroup, Col, ControlLabel, FormControl,  Button} from 'react-bootstrap'
-import UserPostList from './UserPostList';
 import axios from "axios"
 
 class UserProfile extends Component{
   constructor(props ) {
     super(props );
-    this.state = {fullname: "", date: "", city: "", img: ""}
+    this.state = {fullname: "", date: "", city: "", img: "", }
   }
     componentDidMount(){
    
@@ -17,35 +16,46 @@ class UserProfile extends Component{
           var data = response.data.data
           console.log (data)
          this.setState({ 
-           fullname: (data.fullname === undefined)?"Christina":data.fullname, 
+           fullname: (data.fullname === undefined)?"":data.fullname, 
            date: (data.date === undefined)?"":new Date (data.date).toLocaleDateString("en-US"),
-           city: (data.city === undefined)?"San Francisco":data.city,
+           city: (data.city === undefined)?"":data.city,
            img: (data.profilePic === undefined)?"":data.profilePic,
          })
-        })
-      
-  
+        }) 
+      }
+
+handleChange=(e)=>{
+  console.log (e.target.value)
+ if (e.target.type === "text") {
+   this.setState ({fullname: e.target.value})
+ }
+ else {
+  this.setState ({city: e.target.value})
+ }
+}
+handleUpdate=(e)=>{
+  axios.put('http://localhost:3001/user/edit/fullname',{
+      fullname: this.state.fullname,
+      token: localStorage.getItem("token")
+  } )
+  .then( response => {
+    console.log(response);
+ })    
+ .catch(function(error){
+     console.log(error);
+ })
 }
 
   /*
-this.users.fullname
-this.users.joinDate
-this.users.city
-
-
     this.state = {value: ''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleUpdate.bind(this);
   }
-  */
 
-  handleChange(event) {
+    handleChange(event) {
     this.setState({value: event.target.value});
   }
-  handleUpdate(event) {
-    alert('Updated' + this.state.value);
-    event.preventDefault();
-  }
+  */
 
     render(){
       console.log (this.state)
@@ -58,31 +68,39 @@ this.users.city
         </FormGroup>
 
         <FormGroup controlId="formHorizontalName">
-          <Col componentClass={ControlLabel} sm={6}> Name </Col>
-          <Col sm={6}>{ this.state.fullname } </Col>
-        </FormGroup>
-
-        <FormGroup controlId="formHorizontalName">
           <Col componentClass={ControlLabel} sm={6}> Join Date</Col>
           <Col sm={6}>{ this.state.date } </Col>
         </FormGroup>
 
         <FormGroup controlId="formHorizontalName">
+          <Col componentClass={ControlLabel} sm={6}> Name </Col>
+          <Col sm={6}>
+          <FormControl type="text" placeholder="Fullname" value={this.state.fullname } onChange={this.handleChange} /></Col>
+
+
+
+        </FormGroup>
+
+        <FormGroup controlId="formHorizontalName">
           <Col componentClass={ControlLabel} sm={6}> City </Col>
-          <Col sm={6}>{ this.state.city } </Col>
-        </FormGroup>
+          
 
-{/*
-        <FormGroup controlId="formHorizontalEmail">
-            <Col componentClass={ControlLabel} sm={6}>Current City</Col>
-            <Col sm={6}>
-            <FormControl type="text" placeholder="Current City" value={this.state.value} onChange={this.handleChange} />
-            </Col>
-        </FormGroup>
-*/}
-         <Button form="updateUser" onClick={this.handleUpdate} >Update</Button>
+          <Col sm={6}>
+      
+          <FormControl componentClass="select" placeholder="select" onChange={this.handleChange} >
+                <option value="1">London</option>
+                <option value="2">San Francisco</option>
+                <option value="3">Sydney</option>
+                <option value="4">Seattle</option>
+                <option value="5">New York</option>
+          </FormControl>
+          </Col>
 
+          </FormGroup>
+         
+        {/*  <Col sm={6}>{ this.state.city.id } </Col> */}
 
+        <Button form="updateUser" onClick={this.handleUpdate} >Update</Button>    
       </Form>
        
         )
