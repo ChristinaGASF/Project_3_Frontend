317040813f34a10dc0d8ab4271f61193a064ee61
ChristinaGASF <44411722+ChristinaGASF@users.noreply.github.com>
2019-01-15 22:33:10 -0800

    Adjusted font sizes and alignment

--- wayfarer/src/container/Home.js
+++ wayfarer/src/container/Home.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react'
 import Hero from '../components/Header/Hero'
-import {Col, Row, Grid } from 'react-bootstrap'
+import { Col, Row, Grid } from 'react-bootstrap'
+import { Card, CardHeader, CardBody, CardText,} from 'reactstrap'
 
 class Home extends Component{
     render(){
         return(
-            <React.Fragment>
-            <Hero />
-            <div>
-                <h4>Classic Traveler is a travel site for users to share tips about their favorite museums, historical sites and  landmark iconic city spots. </h4>
-            </div>
-            <Grid>
-                <Row className="show-grid">
-                <Col xs={6} md={4}>
-                <h5>Museums</h5>
-                <p>There are many types of museums, including art museums, natural history museums, science museums, war museums, and children's museums. </p>
-                </Col>
-                <Col xs={6} md={4}>
-                <h5>Historical Sites</h5>
-                <p>Historic sites are official locations where pieces of political, military, cultural, or social history have been preserved due to their cultural heritage value.</p>
-                </Col>
-                <Col xs={6} md={4}>
-                <h5>Landmarks</h5>
-                <p>A landmark is a recognizable natural or artificial feature used for navigation, a feature that stands out from its near environment and is often visible from long distances.</p>
-                </Col>
-                </Row></Grid>
-            </React.Fragment>
+        <React.Fragment>
+
+        <Hero />
+            
+
+        <Grid>
+        <Row className="show-grid">
+        <Col md={1}></Col>
+        <Col md={10}>
+            <Card body className="text-center">
+                <h2><b>Classic Traveler</b> is a travel site for users to share tips about their favorite museums, historical sites and  landmark iconic city spots. </h2>
+                </Card>
+            </Col>
+        <Col md={1}></Col>
+        </Row>
+        </Grid>
+
+        <Grid>
+        <Row className="show-grid">
+
+        <Col xs={6} md={4}>
+        <Card>
+            <CardHeader body className="text-center" tag="h3">Museums</CardHeader>
+                <CardBody>
+                    <CardText body className="text-center"><p>There are many types of museums, including art museums, natural history museums, science museums, war museums, and children's museums. </p></CardText>
+                </CardBody>
+            </Card>
+        </Col>
+        
+        <Col xs={6} md={4}>
+        <Card>
+            <CardHeader body className="text-center" tag="h3">Historical Sites</CardHeader>
+                <CardBody>
+                    <CardText body className="text-center"><p>Historic sites are official locations where pieces of political, military, cultural, or social history have been preserved due to their cultural heritage value.</p></CardText>
+                </CardBody>
+            </Card>
+        </Col>
+
+        
+        <Col xs={6} md={4}>
+            <Card>
+            <CardHeader body className="text-center" tag="h3">Landmarks</CardHeader>
+                <CardBody>
+                    <CardText body className="text-center"><p>A landmark is a recognizable natural or artificial feature used for navigation, a feature that stands out from its near environment and is often visible from long distances.</p></CardText>
+                </CardBody>
+            </Card>
+        </Col>
+
+
+        </Row>
+        </Grid>
+        </React.Fragment>
         )
     }
 }
-
 
 export default Home
 
