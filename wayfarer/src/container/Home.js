1f96677798dba2f2b6a210c1f1ddd6b58a1a78f1
ChristinaGASF <44411722+ChristinaGASF@users.noreply.github.com>
2019-01-15 22:48:34 -0800

    added wells to distinguish cards

--- wayfarer/src/container/Home.js
+++ wayfarer/src/container/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Hero from '../components/Header/Hero'
-import { Col, Row, Grid } from 'react-bootstrap'
+import { Well, Col, Row, Grid } from 'react-bootstrap'
 import { Card, CardHeader, CardBody, CardText,} from 'reactstrap'
 
 class Home extends Component{
@@ -10,7 +10,7 @@ class Home extends Component{
 
         <Hero />
             
-
+        <Well>
         <Grid>
         <Row className="show-grid">
         <Col md={1}></Col>
@@ -22,41 +22,49 @@ class Home extends Component{
         <Col md={1}></Col>
         </Row>
         </Grid>
+        </Well>
 
         <Grid>
         <Row className="show-grid">
-
-        <Col xs={6} md={4}>
-        <Card>
-            <CardHeader body className="text-center" tag="h3">Museums</CardHeader>
-                <CardBody>
-                    <CardText body className="text-center"><p>There are many types of museums, including art museums, natural history museums, science museums, war museums, and children's museums. </p></CardText>
-                </CardBody>
-            </Card>
-        </Col>
         
         <Col xs={6} md={4}>
-        <Card>
-            <CardHeader body className="text-center" tag="h3">Historical Sites</CardHeader>
-                <CardBody>
-                    <CardText body className="text-center"><p>Historic sites are official locations where pieces of political, military, cultural, or social history have been preserved due to their cultural heritage value.</p></CardText>
-                </CardBody>
+            <Card>
+                <Well>
+                    <CardHeader body className="text-center" tag="h3">Museums</CardHeader>
+                    <CardBody>
+                        <CardText body className="text-center"><p>There are many types of museums, including art museums, natural history museums, science museums, war museums, and children's museums. </p></CardText>
+                    </CardBody>
+                </Well>
+            </Card>
+        </Col>
+       
+        
+        <Col xs={6} md={4}>
+            <Card>
+                <Well>
+                    <CardHeader body className="text-center" tag="h3">Historical Sites</CardHeader>
+                    <CardBody>
+                         <CardText body className="text-center"><p>Historic sites are official locations where pieces of political, military, cultural, or social history have been preserved due to their cultural heritage value.</p></CardText>
+                    </CardBody>
+                </Well>
             </Card>
         </Col>
 
         
         <Col xs={6} md={4}>
             <Card>
-            <CardHeader body className="text-center" tag="h3">Landmarks</CardHeader>
-                <CardBody>
-                    <CardText body className="text-center"><p>A landmark is a recognizable natural or artificial feature used for navigation, a feature that stands out from its near environment and is often visible from long distances.</p></CardText>
-                </CardBody>
+                <Well>
+                    <CardHeader body className="text-center" tag="h3">Landmarks</CardHeader>
+                    <CardBody>
+                            <CardText body className="text-center"><p>A landmark is a recognizable natural or artificial feature used for navigation, a feature that stands out from its near environment and is often visible from long distances.</p></CardText>
+                    </CardBody>
+                </Well>
             </Card>
         </Col>
 
 
-        </Row>
-        </Grid>
+                </Row>
+            </Grid>
         </React.Fragment>
         )
     }
